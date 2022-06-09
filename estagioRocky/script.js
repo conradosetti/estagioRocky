69eae08ec82b09f1570ff86da177bc045659c7cd
Conrado Setti de almeida Marcelo Oliveira <conrado.setti7@gmail.com>
2022-06-08 22:42:31 -0300

    Add files via upload

---
+++ estagioRocky/script.js
@@ -0,0 +1,12 @@
+        function mOverC()
+        {
+            document.getElementById("texto3").innerHTML = "Conrado Setti <br> Engenheiro da Computação";
+            document.getElementById("texto3").style.display = "inline";
+        }
+
+        function mOutC()
+        {
+            document.getElementById("texto3").style.display = "none";
+        }
+
+       
